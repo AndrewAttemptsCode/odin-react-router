e108bec6b79b54e61b3a333694d4a9528bd00e88
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-12-18 15:18:14 +0000

    refactor: create own file for routes and pass in to router

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import './index.css'
-import App from './App.jsx'
-import Profile from './components/Profile.jsx';
-import Spinach from './components/Spinach.jsx';
-import Popeye from './components/Popeye.jsx';
-import DefaultProfile from './components/DefaultProfile.jsx';
-import ErrorPage from './components/ErrorPage.jsx';
+import routes from "./routes";
+import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "profile",
-    element: <Profile />,
-  },
-  {
-    path: "profile/:name",
-    element: <Profile />,
-  },
-])
+const router = createBrowserRouter(routes);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
