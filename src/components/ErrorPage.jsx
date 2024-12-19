daf09a323623c50f7cd4456d558e0ab74a6dc1da
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-12-19 14:25:29 +0000

    reactor: include display of error message with useRouteError

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage= () => {
+  const error = useRouteError();
+
   return (
     <div>
       <h1>Ut oh, this route does not exist!</h1>
+      <i>{error.statusText || error.message}</i>
       <Link to="/">You may return home, by clicking here though</Link>
     </div>
   );
