2cb1fb58b4fbe98118dc7d6bf387198bb87a2dc4
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-12-19 14:07:09 +0000

    feat: add route

---
+++ src/components/Wimpy.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Wimpy = () => {
+  return (
+    <>
+      <p>Hello, I am Wimpy. I love hamburgers</p>
+      <Link to="/">Click here to go back</Link>
+    </>
+  );
+}
+
+export default Wimpy;
